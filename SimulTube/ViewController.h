d3380fae927016fc20583d4e548e34213f3aa239
Cam <zhouweiyu@WL-200-135.WPA.Claremont.Edu>
2014-11-14 19:33:56 -0800

    Initial Commit

---
+++ SimulTube/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimulTube
+//
+//  Created by Cam on 11/14/14.
+//  Copyright (c) 2014 SimulTube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimulTube/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimulTube
+//
+//  Created by Cam on 11/14/14.
+//  Copyright (c) 2014 SimulTube. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
